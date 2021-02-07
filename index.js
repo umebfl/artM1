106e1df68b50c8d7717eb4ce7f61bce363b9feb3
huanganqi <1079255145@qq.com>
2021-02-08 01:22:51 +0800

    fix

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
+import {AppRegistry} from 'react-native'
+import App from './App'
+import {name as appName} from './app.json'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { YellowBox } from 'react-native'
 
-AppRegistry.registerComponent(appName, () => App);
+YellowBox.ignoreWarnings([
+    'Warning: componentWillMount is deprecated',
+    'Warning: componentWillReceiveProps is deprecated',
+    'Module RCTImageLoader requires',
+    'Warning: componentWillUpdate is deprecated',
+    'ReactNativeFiberHostComponent',
+    'Task orphaned for request',
+]);
+
+AppRegistry.registerComponent(appName, () => App)
